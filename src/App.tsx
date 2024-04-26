d0321f1fa9c5153e9d274be2075116c301c6c35d
bethmij <bethmij@gmail.com>
2024-04-26 11:26:20 +0530

    app.tsx added

---
+++ src/App.tsx
@@ -0,0 +1,49 @@
+// import Message from "./Message.tsx";
+// import Button from "./Button.tsx";
+// import Alert from "./Alert.tsx";
+// import {useState} from "react";
+
+
+// const listName = ["one", "two", "three", "four", "five"]
+
+// const handleItem = () => {
+//     console.log(item)
+// }
+
+// eslint-disable-next-line react-hooks/rules-of-hooks
+
+// import Bug from "./Bug.tsx";
+// import Game from "./Game.tsx";
+// import Pizza from "./Pizza.tsx";
+
+// import Expandable from "./Expandable.tsx";
+import Form from "./Form.tsx";
+
+export default function App() {
+
+    // const [alertVisible, setAlertVisibility] = useState(false)
+
+    return (
+        <>
+            {/*<IoHome color={"red"} size={40}/>*/}
+            {/*<div><Message items={listName} heading="Cities" onSelectItem={handleItem}></Message></div>*/}
+            {/*{alertVisible && <Alert onClick={() =>setAlertVisibility(false)}><strong>Holy guacamole!</strong> You should check in on some of those fields below.</Alert>}*/}
+            {/*<Button onClick = {() => setAlertVisibility(true)} >My Button</Button>*/}
+            {/*<Button></Button>*/}
+            {/*<Bug></Bug>*/}
+            {/*<Game></Game>*/}
+            {/*<Pizza></Pizza>*/}
+            {/*<Expandable maxChar={400} >*/}
+            {/*    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus asperiores aspernatur assumenda*/}
+            {/*    consequuntur dolorum eius, error et excepturi impedit incidunt laborum modi natus nisi optio porro*/}
+            {/*    possimus quidem quod ratione rerum sed sit, veniam voluptas voluptatum! Delectus ex illum iure*/}
+            {/*    laboriosam quasi reprehenderit repudiandae temporibus? Adipisci ducimus eius incidunt magnam optio quas*/}
+            {/*    reiciendis, voluptatum. Adipisci consequuntur cumque eligendi illo in, inventore iusto laboriosam libero*/}
+            {/*    placeat quas, quo ratione saepe voluptates! Commodi deserunt ea eius, ex laboriosam magni modi nobis*/}
+            {/*    odio quaerat suscipit tenetur vel voluptas voluptate? Alias, aliquid animi atque iste natus nesciunt*/}
+            {/*    repudiandae sequi tempore. Dolorem laboriosam optio veniam?*/}
+            {/*</Expandable>*/}
+            <Form></Form>
+        </>
+    )
+}
